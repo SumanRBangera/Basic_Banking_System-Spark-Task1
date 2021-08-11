2ae1bdcbca6b141a25452483b18b7579ac614857
SumanRBangera <70790498+SumanRBangera@users.noreply.github.com>
2021-08-11 12:49:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard</title>
+<link rel="stylesheet" type="text/css" href="dash.css">
+</head>
+<nav class="topnav">
+ <h2 class="logo">SPARK BANK</h2>
+  <a href="transaction.php" class="transaction">TRANSACTION HISTORY</a>
+  <a href="transfer.php" class="transfer">TRANSFER MONEY</a>
+   <a href="customer.php" class="customer">USER</a>
+  <a href="index.php" class="dash">HOME</a>
+</nav>
+<div class="row intro py-1" style="background-color : #17a2b8;">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h1>WELCOME TO SPARK BANK </h1>
+                  
+                </div>
+              </div>
+</div>
+<style>
+  p{
+font-size: 25px;
+font-weight: bold;
+margin-left: 26cm;
+ color: black;
+ padding: 1cm;
+}
+h1{
+  text-align: center;
+  font-size: 1cm;
+}
+body {font-family: Arial, Helvetica, sans-serif;
+             background-repeat: no-repeat;
+              font-family: "Times New Roman";
+            
+           
+  background-size: cover;}
+* {box-sizing: border-box;
+}
+html{ background-image: url(bank.jpg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: 70% 130%}
+ 
+.btn1 {
+  background-color: #FFA500;
+  color: white;
+  padding: 16px 20px;
+  text-align: center;
+  border: none;
+  cursor: pointer;
+  width: 7.5cm;
+  margin-bottom:2px;
+  opacity: 0.8;
+ 
+  margin-top: 0.2cm;
+}
+table, th {
+ 
+  border: 1px solid white;
+  padding: .2cm;
+ width: 10cm;
+ background:black;
+ margin-right: 3cm;
+ color: white;
+font-weight: bold;}
+.heading{
+   font-weight: bolder;
+   padding-top: 10%;
+   padding-left: 55%;
+   font-size: 30px;
+   color: white;
+}
+</style>
+<body>
+  <div class="heading">
+        Welcome to our bank - <br />
+              SUMAN
+        <h3>Get Easy and Secure Transactions Here!!</h3>
+      </div>
+  <center>
+ <form method="POST" action="" enctype="multipart/form-data">
+<br>
+<br>
+<br>
+</form>
+</center>
+<footer class="text-center mt-5 py-2">
+        <<p>&copy 2021. Made by <b>SUMAN</b> <br> SPARK FOUNDATION</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
